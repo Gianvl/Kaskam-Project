376a356f9d1c886f60329f6b9a2a13dabfbb2565
Gianfranco <89668555+Gianvl@users.noreply.github.com>
2022-11-24 22:09:07 -0300

    Update Character.h

--- Character.h
+++ Character.h
@@ -3,61 +3,77 @@
 
 #include "Weapon.h"
 #include "Armor.h"
+#include "Enemy.h"
 
 class Character
 {
+    private:
+        string _name;
+        char _sex;
+
+    protected:
+        string _className;
+        Weapon* _weapon;
+        Armor* _armor;
+        int _fights;
+        int _life;
+        int _lifeMax;
+        int _stamina;
+        int _staminaMax;
+        int _kenkens;
+        int _kenkensMax;
+        int _smarfons;
+        int _smarfonsMax;
+        int _gold;
+        int _points;
+
     public:
 
-        // CONSTRUCTORS & DESTRUCTORS
+        ///CONSTRUCTORS & DESTRUCTORS
         Character(const string& name, const char sex);
-        virtual ~Character();
+        virtual inline ~Character(){}
 
-        // METODS
+        ///METODS
+
+        //COMMONS
         void show()const;
-        char getSex()const;
-        int getFights()const;
+        void travel();
+        void event();
+        void rest();
+        //FIGHTS
+        void battle(Enemy* en);
+        void yourTurn(Enemy* en);
+        //void boss();
+        Enemy* enemyGen()const;
+        //SHOP
+        void shop();
+        int shopSpeach()const;
+        Armor* armorStock()const;
+        Weapon* weaponStock()const;
 
-        int attack();
-        void defend(int dmg);
-        void hurt(int dmg);
-        int getLife()const;
+        //UTILITIES
+        inline char getSex()const {return _sex;}
+        inline int getFights()const {return _fights;}
+        inline int getLife()const {return _life;}
+        inline float attack(){return _weapon->getDmg();}
+        void defend(float dmg);
+        void inline hurt(float dmg)
+        {cout << "FUCKKK!!" << endl; _life -= dmg;}
         void heal(int lp);
         void energyUp(int ep);
         void exhaust(int ep);
         virtual int smoke();
         virtual int drink();
         int getSmarfon();
-        int getQuenquen();
-
-        virtual int pay(int gold);
-        virtual void buyQuenquen();
-        virtual void buySmarfon();
-        virtual void buyWeapon(Weapon* wp);
-        virtual void buyArmor(Armor* ar);
-        virtual void sellWeapon();
-        virtual void sellArmor();
-
-        //FRIENDS
-        friend class Game;
-
-
-    protected:
-        string name;
-        char sex;
-        string className;
-        Weapon* weapon;
-        Armor* armor;
-        int fights;
-        int life;
-        int lifeMax;
-        int stamina;
-        int staminaMax;
-        int quenquens;
-        int quenquensMax;
-        int smarfons;
-        int smarfonsMax;
-        int gold;
-
+        int getKenken();
+        int inline gold()const{return _gold;}
+        int pay(int gold);
+        void buyKenken();
+        void buySmarfon();
+        void buyWeapon(Weapon* wp);
+        void buyArmor(Armor* ar);
+        void sellWeapon();
+        void sellArmor();
 };
 
 //CLASSES
@@ -74,6 +90,7 @@ class Kaskam : public Character
     public:
         //CONSTRUCTORS & DESTRUCTORS
         Kaskam(const string& name, const char sex);
+        inline ~Kaskam(){}
 
         //METODS
         int smoke();
@@ -85,6 +102,7 @@ class Cristian : public Character
     public:
         //CONSTRUCTORS & DESTRUCTORS
         Cristian(const string& name, const char sex);
+        inline ~Cristian(){}
 
         //METODS
         int drink();
@@ -95,6 +113,7 @@ class Human : public Character
     public:
         //CONSTRUCTORS & DESTRUCTORS
         Human(const string& name, const char sex);
+        inline ~Human(){}
 
 };
 
