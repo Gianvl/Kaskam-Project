48dbb7751b57f03e3ca0fa7ad3f6e77dc36639bf
Gianfranco <89668555+Gianvl@users.noreply.github.com>
2022-11-24 22:06:59 -0300

    Update Armor.cpp

--- Armor.cpp
+++ Armor.cpp
@@ -1,120 +1,52 @@
-#include "Weapon.h"
+#include "Armor.h"
 #include "Game.h"
 
-Weapon::Weapon(const string& name, int damage)
-:_name(name), _damage(damage)
+Armor::Armor(const string& name, int defense)
+: _name(name), _defense(defense)
 {}
 
-//STATIC
-
-Weapon* Weapon::weaponDrop();
+float Armor::getDef()const
 {
-
+    return Game::normalF(_defense, 1.5);
 }
 
-float Weapon::getDmg()const
+void Armor::show()const
 {
-    return Game::normalF(_damage, 3.3);
-}
-
-void Weapon::show()const
-{
-    cout << "WEAPON: "   << _name      << "\t\t"
-         << "DAMAGE: "   << _damage    << endl;
+    cout << "ARMOR: "   << _name      << "\t\t"
+         << "DEFENSE: " << _defense   << endl;
 }
 
 
-//HANDS
+//KSAK
 
-Hands::Hands()
-:Weapon("Hands", 10.0)
+Ksak::Ksak()
+:Armor("K-sa-K", 5.0)
 {}
 
-//MOCHO
 
-Mocho::Mocho()
-:Weapon("Mocho", 42.0)
-{
-    _uses = 2;
-    _usesMax = 2;
-    _ammo = 0;
-    _ammoMax = 2;
-}
+//WIND BREAKER
 
-float Mocho::getDmg()
-{
-    if(_ammo > 0 && _uses > 0)
-    {
-        _uses--;
-        _ammo--;
-        return Weapon::getDmg();
-    }
-
-    else
-        cout << (_uses <= 0 ? "WEAPON BROKEN!" : "NO AMMO!") << endl;
-    return 0;
-}
-
-void Mocho::show()const
-{
-    Weapon::show();
-    cout << "AMMO: "      << _ammo  << "\t\t";
-    cout << "USES LEFT: " << _uses  << endl;
-}
-
-
-//SWORD OF ARUZOR
-
-swordOfAruzor::swordOfAruzor()
-:Weapon("Sword Of Aruzor", 32.0)
+WindBreaker::WindBreaker()
+:Armor("Wind Breaker", 9.0)
 {
     _uses = 5;
     _usesMax = 5;
 }
 
-float swordOfAruzor::getDmg()
+float WindBreaker::getDef()
 {
     if(_uses <= 0)
     {
-        cout << "WEAPON BROKEN!" << endl;
+        cout << "ARMOR BROKEN!" << endl;
         return 0;
     }
     _uses--;
-    return Weapon::getDmg();
+    return Armor::getDef();
 }
 
-void swordOfAruzor::show()const
+void WindBreaker::show()const
 {
-    Weapon::show();
+    Armor::show();
     cout << "USES LEFT: " << _uses << endl;
 }
-
-
-//KaskOT
-
-KaskOT::KaskOT()
-:Weapon("Kask-OT", 18.5)
-{
-    _uses = 3;
-    _usesMax = 5;
-}
-
-float KaskOT::getDmg()
-{
-    if(_uses > 0)
-    {
-        _uses--;
-        return Weapon::getDmg();
-    }
-
-    cout << "NO AMMO" << endl;
-    return 0;
-}
-
-void KaskOT::show()const
-{
-    Weapon::show();
-    cout << "USES LEFT: " << _uses << endl;
-}
-
 
